00649e7299cbbff6d9d61526bcab1ace755339c1
tranhuytoan161104 <176930448+tranhuytoan161104@users.noreply.github.com>
2025-06-25 06:07:14 +0700

    Modify OrderStatus Enum Class

---
+++ Final.Domain/Enums/EOrderStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final.Domain.Enums
+{
+    public enum EOrderStatus
+    {
+        Pending,
+        Processing,
+        Shipped,
+        Delivered,
+        Cancelled
+    }
+}
